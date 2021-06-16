c64a9273fcec664ac0fa132bbfe2f38db50026a8
Daniel Burke <burkey@burkeez.com>
2021-06-16 19:57:09 +1000

    Added license.

--- routecheck/main.cpp
+++ routecheck/main.cpp
@@ -1,11 +1,31 @@
 /*
 	routecheck - check the route to a destination and report which of the specified routes were encountered.
 
-	Written by Daniel Burke, 2021
-
 	This was designed to check for which gateway is active in a redundantly linked environment.
 
 	This is a functional example of the Windows IcmpSendEcho functions to trace a route.
+
+	MIT License
+
+	Copyright (c) 2001 Daniel Burke
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
 
 */
 
